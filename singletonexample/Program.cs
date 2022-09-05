52c7a017dee843bd60651a1fcedcbe31f77a4583
Kris Kristensen <kbk1303@gmail.com>
2022-09-05 11:38:03 +0200

    Add project files.

---
+++ singletonexample/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace singleton.example
+{
+    internal class TestSIngleTon
+    {
+        private static TestSIngleTon? instance = null;
+        private TestSIngleTon() { }
+
+        public static TestSIngleTon Instance
+        {
+            get
+            {
+                instance ??= new TestSIngleTon();//compound statement ??= same as nullable
+                return instance;
+            }
+        }
+    }
+
+}
